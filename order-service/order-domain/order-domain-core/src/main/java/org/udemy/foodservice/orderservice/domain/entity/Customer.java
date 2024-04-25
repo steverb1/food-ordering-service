9c55531596a87247942a9c4d6ff776a7df60bd1f
steve.bement <steve.bement@accenture.com>
2024-04-25 15:25:50 -0600

    Adding state chaning methods to Order entity (lesson 16).

---
+++ order-service/order-domain/order-domain-core/src/main/java/org/udemy/foodservice/orderservice/domain/entity/Customer.java
@@ -0,0 +1,7 @@
+package org.udemy.foodservice.orderservice.domain.entity;
+
+import org.udemy.foodservice.domain.entity.AggregateRoot;
+import org.udemy.foodservice.domain.valueobject.CustomerId;
+
+public class Customer extends AggregateRoot<CustomerId> {
+}
