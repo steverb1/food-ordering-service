640d4bb8f1290f628e88c46f9cc7e038e55c333b
steve.bement <steve.bement@accenture.com>
2024-04-29 13:15:20 -0600

    Implementing Order Domain Service (lesson 18).

--- order-service/order-domain/order-domain-core/src/main/java/org/udemy/foodservice/orderservice/domain/OrderDomainService.java
+++ order-service/order-domain/order-domain-core/src/main/java/org/udemy/foodservice/orderservice/domain/OrderDomainService.java
@@ -1,17 +1,73 @@
 package org.udemy.foodservice.orderservice.domain;
 
+import lombok.extern.slf4j.Slf4j;
 import org.udemy.foodservice.orderservice.domain.entity.Order;
+import org.udemy.foodservice.orderservice.domain.entity.Product;
 import org.udemy.foodservice.orderservice.domain.entity.Restaurant;
 import org.udemy.foodservice.orderservice.domain.event.OrderCancelledEvent;
 import org.udemy.foodservice.orderservice.domain.event.OrderCreatedEvent;
 import org.udemy.foodservice.orderservice.domain.event.OrderPaidEvent;
+import org.udemy.foodservice.orderservice.domain.exception.OrderDomainException;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.List;
 
-public interface OrderDomainService {
-    OrderCreatedEvent validateAndInitiateOrder(Order order, Restaurant restaurant);
-    OrderPaidEvent payOrder(Order order);
-    void approveOrder(Order order);
-    OrderCancelledEvent cancelOrderPayment(Order order, List<String> failureMessages);
-    void cancelOrder(Order order, List<String> failureMessages);
+@Slf4j
+public class OrderDomainService implements ForProcessingOrders {
+
+    private static final String UTC = "UTC";
+
+    @Override
+    public OrderCreatedEvent validateAndInitiateOrder(Order order, Restaurant restaurant) {
+        validateRestaurant(restaurant);
+        setOrderProductInformation(order, restaurant);
+        order.validateOrder();
+        order.initializeOrder();
+        log.info("Order with id: {} is initiated", order.getId().getValue());
+        return new OrderCreatedEvent(order, ZonedDateTime.now(ZoneId.of(UTC)));
+    }
+
+    @Override
+    public OrderPaidEvent payOrder(Order order) {
+        order.pay();
+        log.info("Order with id: {} is paid", order.getId().getValue());
+        return new OrderPaidEvent(order, ZonedDateTime.now((ZoneId.of(UTC))));
+    }
+
+    @Override
+    public void approveOrder(Order order) {
+        order.approve();
+        log.info("Order with id: {} is approved", order.getId().getValue());
+    }
+
+    @Override
+    public OrderCancelledEvent cancelOrderPayment(Order order, List<String> failureMessages) {
+        order.initCancel(failureMessages);
+        log.info("Order payment is cancelling for order id: {}", order.getId().getValue());
+        return new OrderCancelledEvent(order, ZonedDateTime.now(ZoneId.of(UTC)));
+    }
+
+    @Override
+    public void cancelOrder(Order order, List<String> failureMessages) {
+        order.cancel(failureMessages);
+        log.info("Order with id: {} is cancelled", order.getId().getValue());
+    }
+
+    private void validateRestaurant(Restaurant restaurant) {
+        if (!restaurant.isActive()) {
+            throw new OrderDomainException("Restaurant with id " + restaurant.getId().getValue() +
+                    " is currently not active!");
+        }
+    }
+
+    private void setOrderProductInformation(Order order, Restaurant restaurant) {
+        order.getItems().forEach(orderItem -> restaurant.getProducts().forEach(restaurantProduct -> {
+            Product currentProduct = orderItem.getProduct();
+            if (currentProduct.equals(restaurantProduct)) {
+                currentProduct.updateWithConfirmedNameAndPrice(restaurantProduct.getName(),
+                        restaurantProduct.getPrice());
+            }
+        }));
+    }
 }
